ba7461a5c9789132b2814ffa779746ec32c8fc42
sohyang <jung.sohyang@gmail.com>
2026-08-13 17:13:54 +0900

    solve: [SWEA 3499] 퍼펙트 셔플 (D3)

---
+++ cheese-flavor/week-01/swea_3499_퍼펙트 셔플.py
@@ -0,0 +1,33 @@
+# SWEA 3499. 퍼펙트 셔플
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWGsRbk6AQIDFAVW
+# 소요시간: 15분 / 시도: 1회
+
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr.append(input())
+    
+    sector = n//2 #구조를 분리할 값
+    
+    middle, up, down = 0, 0, 0    #중
+    for i in range(n):
+        middle += int(arr[sector][i])    
+        
+    if n != 1:
+        up = int(arr[0][sector])   #상
+        for i in range(1, sector):
+            up += int(arr[i][sector])
+
+            for j in range(1, i+1):
+                up += int(arr[i][sector-j]) + int(arr[i][sector+j])
+
+        down = int(arr[n-1][sector])  #하
+        for i in range(n-2, sector, -1):
+            down += int(arr[i][sector])
+            
+            for j in range(1, n-i):
+                down += int(arr[i][sector-j]) + int(arr[i][sector+j])
+        
+    print(f"#{test_case} {up+down+middle}")
