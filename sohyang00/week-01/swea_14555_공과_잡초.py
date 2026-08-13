5b3b934c23f9944a091d564d3758df64e73d2cd7
jung.sohyang <jung.sohyang@gmail.com>
2026-08-13 14:31:21 +0900

    refactor: [SWEA 14555] 탐색  방식 변경 stack -> 인접 문자 기반

--- sohyang00/week-01/swea_14555_공과_잡초.py
+++ sohyang00/week-01/swea_14555_공과_잡초.py
@@ -9,31 +9,11 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     arr = input().strip()
-    stack = []
     count = 0
 
-    for i in arr:
-        if i == '(': # 굳이... 처리를 해야할까? 걍 다 넣자
-            stack.append('(')
-        elif i == ')':
-            if not stack: 
-                continue
-            else: 
-                tmp = stack.pop()
-                if tmp == '(':
-                    count += 1
-                elif tmp == '|':
-                    count += 1
-        elif i == '|':
-            if not stack: 
-                stack.append('|')
-            else: 
-                tmp = stack.pop()
-                if tmp == '(':
-                    count += 1
-                elif tmp == '|':
-                    stack.append('|')
-        else: # . 일때 ... 
-            continue
+    for i in range(len(arr) - 1):
+        if arr[i:i+2] in ('()', '(|', '|)'):
+            count += 1
+
     print(f'#{test_case} {count}')
 
