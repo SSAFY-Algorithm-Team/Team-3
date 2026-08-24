5474e4c4e78c7a2f6cecaf56c7f3366a37f2d32f
zoo0o <lucwwky@gmail.com>
2026-08-24 14:40:32 +0900

    solve: [PGS LV3] 네트워크

---
+++ zoo0o/week-03/pgs_lv3_네트워크.py
@@ -0,0 +1,22 @@
+# 프로그래머스 Lv3. 네트워크
+# https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3
+# 소요시간: 1시간 / 시도: 3회
+
+
+def solution(n, computers):
+    answer = 0
+    visited = [0] * n
+
+    def dfs(current):
+        visited[current] = 1
+
+        for i in range(n):
+            if computers[current][i] == 1 and visited[i] == 0:
+                dfs(i)
+
+    for i in range(n):
+        if visited[i] == 0:
+            answer += 1
+            dfs(i)
+
+    return answer
