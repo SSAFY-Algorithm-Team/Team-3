aabd77b2d9eb75278e9d91441af9e06ff01e58dd
gxuoo <rbdud189@naver.com>
2026-09-04 13:30:33 +0900

    solve: [SQL Lv1] 상위 n개 레코드

---
+++ gxuoo/week-05/sql_lv1_상위_n개_레코드.sql
@@ -0,0 +1,6 @@
+-- 프로그래머스 SQL Lv1. 상위 n개 레코드
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59405
+-- 소요시간: 1분 / 시도: 1회
+
+-- 코드를 입력하세요
+SELECT NAME FROM ANIMAL_INS ORDER BY DATETIME LIMIT 1;
