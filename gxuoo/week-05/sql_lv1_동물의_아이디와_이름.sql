b497580adf432caee7972b62f3b24522ee798a24
gxuoo <rbdud189@naver.com>
2026-09-04 13:30:32 +0900

    solve: [SQL Lv1] 동물의 아이디와 이름

---
+++ gxuoo/week-05/sql_lv1_동물의_아이디와_이름.sql
@@ -0,0 +1,6 @@
+-- 프로그래머스 SQL Lv1. 동물의 아이디와 이름
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59403
+-- 소요시간: 1분 / 시도: 1회
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS ORDER BY ANIMAL_ID;
